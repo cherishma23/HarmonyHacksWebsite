a253c70b94b329dfe257dd776739b304dca542ba
cherishma23 <115597751+cherishma23@users.noreply.github.com>
2025-04-28 01:01:28 -0700

    initial push after fork

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // <-- changed
 import { NavbarBootstrap } from './components/NavbarBS';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -16,36 +16,26 @@ import Hackathon from './pages/Hackathon';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router> {/* Now HashRouter */}
       <ScrollToTop />
       <MusicProvider>
-        <Music/>
-      <Routes>
-        {/* Public routes */}
-        <Route
-          path="/*"
-          element={
-            <>
-              <NavbarBootstrap />
-              <div className="d-flex flex-column min-vh-100">
-                <main className="flex-grow-1">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/events" element={<Events />} />
-                    <Route path="/resources" element={<Resources />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/earlyMilestones" element={<EarlyMilestones />} />
-                    <Route path="/aboutMusic" element={<AboutMusic />} />
-                    <Route path="/hackathon" element={<Hackathon />} />
-                  </Routes>
-                </main>
-                <Footer />
-              </div>
-            </>
-          }
-        />
-      </Routes>
+        <Music />
+        <div className="d-flex flex-column min-vh-100">
+          <NavbarBootstrap />
+          <main className="flex-grow-1">
+            <Routes> {/* Only one <Routes> */}
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/resources" element={<Resources />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/earlyMilestones" element={<EarlyMilestones />} />
+              <Route path="/aboutMusic" element={<AboutMusic />} />
+              <Route path="/hackathon" element={<Hackathon />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </MusicProvider>
     </Router>
   );
