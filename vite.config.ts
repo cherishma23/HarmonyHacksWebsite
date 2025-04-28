4e1475ba3b723979851922fb3fd301f9fb222709
cherishma23 <115597751+cherishma23@users.noreply.github.com>
2025-04-27 23:57:40 -0700

    initial push after fork

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/HarmonyHacksWebsite/', // 👈 ADD THIS LINE!
   server: {
     host: '0.0.0.0',
     proxy: {
